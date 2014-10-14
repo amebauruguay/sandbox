277fa6030536059de2724d060fbd0e5a5a78e218
Miguel Vallvé <mvallve@ameba.com.uy>
2014-10-13 22:38:18 -0200

    aclaraciones en el arhivo

--- php/calc/index2.php
+++ php/calc/index2.php
@@ -1,118 +1,159 @@
-<? if(isset($_POST['que']) && $_POST['que']!==''){$que = $_POST['que'];}else {$que='';} ?>
+<? 
+    // verifico que se haya enviado algo por el formulario y no sea vacio.
+	if(isset($_POST['que']) && $_POST['que']!==''){$que = $_POST['que'];}else {$que='';} 
 
+	// funcion para ver los arrays en pantalla
+	function pp($arr, $titulo = ''){
+		echo '<pre>'.$titulo;
+		print_r($arr);
+		echo '</pre><hr>';
+	}
+
+?>
+
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<meta name="description" content="Free Web tutorials">
-<meta name="keywords" content="HTML,CSS,XML,JavaScript">
-<meta name="author" content="Hege Refsnes">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Calculadora basica</title>
+	<style>
+		body{margin:auto; width:500px; text-align: center; font-family: sans-serif;}
+		.operaciones{display: block;background: grey;margin: -5px 0;color: white;padding: 5px;}
+		input[type="text"]{padding: 5px; border: 1px solid gray; width:397px;}
+		input[type="submit"]{padding: 5px; border: 1px solid gray;background: none;	text-transform: uppercase;}
+		input[type="submit"]:hover{color:white;background: gray;cursor: pointer;}
+		</style>
 </head>
-	<body>
+<body>
 
-	<h1></h1>
+	<h1>Calculin200</h1>
 		<form action="?calculalo" method="post">
-		<input type="text" value="<?= $que ?>" name="que" placeholder="">
-		<input type="submit">
-		</form>
+		<!-- uso el valor de $que para mostrar -->
+		<input type="text" value="<?= $que ?>" name="que" placeholder="Ingrese su operación">
+		<input type="submit" value="calcular">
+	</form>
+	<br>
 
 	<?php 
 
-	// declaro variables;
-	$mensajes = ['Por favor, dame algo para hacer', 'Voy a ', 'Resultado: ', 'Cha chan!'];
+	// declaro variables
+	$mensajes = ['Por favor, dame algo para hacer', 'Resultado: ', 'Cha chan!'];
+	// array asociativo de operaciones
 	$operadores =array('sumar'=>'+','multiplicar'=>'*','restar'=>'-','dividir'=>'/');
+	// indice de mensajes
 	$queDecir = 0;
-	$operacionGuardada[] = 'Operación no válida...';
-
-	// verifico que se haya enviado algo por el formulario y que sea differente de vacio
-	if(isset($_POST['que']) && $_POST['que']!==''){
-
-		// tomo la variable que me pasa por GET
-		$que = $_POST['que'];
-		$queDecir ++;
+	// array de operaciones a ejecutar 
+	$operacionGuardada = array();
+	
+	// si se envió algo... 
+	if($que!==''){
+		
+		// indice para recorrer el string con la operación ($que)
 		$charTolook=1;
+		
+		// indice para guardar los números y operaciones
 		$poss=0;
 
 		//recorro el string que pasaron desde la posicion 1 en adelante: +5/-3+4
+		// Igonoro el caracter en la poscion 0 porque aunque sea un operador no voy a operar con él, por eso definí $charToLool = 1
 		for ($charTolook=1; $charTolook < strlen($que); $charTolook++) {
 
-			// comparo cada caracter con un simbolo pero ignoro el primer caracter porque no voy a operar con el
+			// comparo cada caracter con un operador (+,*,-,/).
 			foreach ($operadores as $operacion => $operador){
 
-			if($que[$charTolook]==$operador) {   // 5 == +, 5==*, 5 == -, 5==/, / == +, /==*, / == -, /==/ Si
-				
-				// guardo la operación en una variable
-				$operacionGuardada[$poss] = $operacion; // 'dividir'
+				// si el caracter es un operador
+				if($que[$charTolook]==$operador) {   // : 5 == + no, 5==* no, 5 == - no, 5==/ no, / == + no, /==* no, / == - no, /==/ SI
+					
+					// lo guardo en $operacionGuardada en posición determinada
+					$operacionGuardada[$poss] = $operacion; // $operacionGuardada[0] = 'dividir'
 
-				//Guardo los dos numeros en un array, ambos estan en $que
-				// El primero esta desde la posicion 0 hasta la posicion del operador 
-				$numeros[$poss] = (integer)substr($que,0,$charTolook);  //  $numeros[0] = +5
-				// re-defino donde buscar a todo lo que sigue 
-				$que = substr($que,$charTolook+1);  // ahora $que vale -3+4
-				// con (integer) al principio me aseguro de convertirlos a numeros matematicamente hablando.
-				$poss++;
-				$charTolook=0;
+					//Guardo el número que esta antes del operador en un array
+					
+					// Convierto a número (integer) lo que encuentre en $que desde la posicion 0 hasta la posicion del operador encontrado
+					// y lo guardo en la posición determinada dentro del array de números 
+					$numeros[$poss] = (integer)substr($que,0,$charTolook);  //  $numeros[0] = +5
+					
+					// re-defino $que para buscar el siguiente operador
+					$que = substr($que,$charTolook+1);  // ahora $que = -3+4 
+					$poss++;  // incremento uno la posición de los arrays donde guardo la información
+					$charTolook=0; // como re-definí la variable $que y le saque la pimera parte lo vuelvo a recorrer desde el principio 
 
-			}
-		}
+				} // termina el if
+			} // termina el foreach
+		} // termina el for 
 
-			
-		}
-
+		// el último número es lo que sea que haya quedado en la variable $que
 		$numeros[$poss] = (integer)$que;
 
-		 /*//para ver el array de numeros	
-			echo '<pre>';
-			print_r($numeros);
-			echo '</pre>';
-		 //para ver el array de operaciones	
-			echo '<pre>';
-			print_r($operacionGuardada);
-			echo '</pre>';
-		 */
+		//descomentar para ver el array de operaciones
+		//pp($operacionGuardada, 'Operaciones: ');
 
+		// Ahora que tenemos un array con números y un array con operaciones, vamos a hacer las cuentas
+		// Esta versión de calculin no respeta la propiedad asociativa. 
+		// Es decir: 5+5*2 = 10*2 = 20 cuando en realidad debería ser 50
+
+		// variable para cambiar de operación 
 		$indiceOperaciones=0;
 
+		// por cada operacion
 		foreach ($operacionGuardada as $opera){
-			if(count($numeros)>1){
-				echo $opera.': '.$numeros[0].' con '.$numeros[1].'</br>';
 
+			// descomentar para ver como va quedan el array de números
+			//pp($numeros, 'Números vez '.$indiceOperaciones.': ');
+
+			// si tengo más de un número
+			if(count($numeros)>1){
+
+				// Digo que voy a hacer
+				echo '<span class="operaciones">'.ucwords($opera).': '.$numeros[0].' con '.$numeros[1].'</span><hr>';
+
+				// En cada caso ejecuto una opreacion diferente
+				// guardando el resultado de la operación en la posicion 1 del array
 				switch ($opera) {
 					case 'sumar':
-						$numeros[0] = $numeros[0]+$numeros[1];
+						$numeros[1] = $numeros[0]+$numeros[1];
 						break;
 					case 'multiplicar':
-						$numeros[0] = $numeros[0]*$numeros[1];
+						$numeros[1] = $numeros[0]*$numeros[1];
 						break;
 					case 'restar':
-						$numeros[0] = $numeros[0]-$numeros[1];
+						$numeros[1] = $numeros[0]-$numeros[1];
 						break;
 					case 'dividir':
-						if($numeros[1]==0){$numeros[0]='Bananas';}
-						else{$numeros[0] = $numeros[0]/$numeros[1];}
+						if($numeros[1]==0){$numeros[1]='Bananas';}
+						else{$numeros[1] = $numeros[0]/$numeros[1];}
 						break;
-					
 					default:
-						$numeros[0] = 'Operación no válida...';
+						$numeros[1] = 'Operación no válida...';
 						break;
 				}
-				$numeros[1] = array_pop($numeros);
-				$indiceOperaciones++;
-			}else{ $numeros[0] = $que.' porque no usaste ningún operador.';}
+
+				// En este momento en el array de números la posicion 1 tiene ahora el resultado de la primer operación
+				// Elimino el elemento de la posición 0 pues ya no lo voy a usar
+				array_shift($numeros);
+
+				// cambio de operación a realizar y vuelvo a empezar
+				$indiceOperaciones++;		
+
+			}else{ 
+				// Mensaje de error por su no usa ningún operador
+				$numeros[0] = $que.' <br><small>no usaste ningún operador.</small>';
+			}// temrina el if
 			
-		}
+		}// termina el foreach
 
+		// Escribo el resultado
 		$queDecir++;
-		echo '<br><h2>'.$mensajes[$queDecir].$numeros[0].'</h2>';
+		echo '<h2>'.$mensajes[$queDecir].$numeros[0].'</h2>';
 		$queDecir ++;
-	}
 
+
+	}// termina el  "si se envió algo..."
+
+	// Escribo "dame algo para hacer" o "Cha Chan!" según si se envió algun dato o no
 	echo $mensajes[$queDecir];
 
-
-
-
-
 	?>
-
 	</body>
 </html>

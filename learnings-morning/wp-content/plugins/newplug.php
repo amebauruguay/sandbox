a0fb0dd4dc3a1b31978084e0ee883edd631d1637
Miguel Vallvé <mvallve@ameba.com.uy>
2015-06-10 10:55:11 -0300

    plug

--- learnings-morning/wp-content/plugins/newplug.php
+++ learnings-morning/wp-content/plugins/newplug.php
@@ -14,25 +14,97 @@ $prefix="mv";
 
 class mv_new_one extends WP_Widget{
 
-	function __construct(){
+  public function __construct() {
+      $widget_ops = array('classname' => 'My_City', 'description' => 'Displays a city!' );
+      $this->WP_Widget('My_City', 'My city', $widget_ops);
+  }
+ 
+  function widget($args, $instance) {
+    // PART 1: Extracting the arguments + getting the values
+    extract($args, EXTR_SKIP);
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+    $city = empty($instance['city']) ? '' : $instance['city'];
+ 
+    // Before widget code, if any
+    echo (isset($before_widget)?$before_widget:'');
+   
+    // PART 2: The title and the text output
+    if (!empty($title))
+      echo $before_title . $title . $after_title;;
+    if (!empty($city))
+      echo $city;
+   
+    // After widget code, if any  
+    echo (isset($after_widget)?$after_widget:'');
+  }
 
-		$params = array(
-			'description'=> 'This is the widget descir',
-			'name'=> 'new Widget',
-		);
+  public function form( $instance ) {
+   
+     // PART 1: Extract the data from the instance variable
+     $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+     $title = $instance['title'];
+     $city = $instance['city'];  
+   
+     // PART 2-3: Display the fields
+     ?>
+     <!-- PART 2: Widget Title field START -->
+     <p>
+      <label for="<?php echo $this->get_field_id('title'); ?>">Title:
+        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
+               name="<?php echo $this->get_field_name('title'); ?>" type="text"
+               value="<?php echo attribute_escape($title); ?>" />
+      </label>
+      </p>
+      <!-- Widget Title field END -->
+   
+     <!-- PART 3: Widget City field START -->
+     <p>
+      <label for="<?php echo $this->get_field_id('text'); ?>">City:
+        <select class='widefat' id="<?php echo $this->get_field_id('city'); ?>"
+                name="<?php echo $this->get_field_name('city'); ?>" type="text">
 
-		parent:: __construct('new Widget','',$params);
+          <option value='New York'<?php echo ($city=='New York')?'selected':''; ?>>
+            New York
+          </option>
+	
+		<?php  
+	    	$return = array();
 
-	}
+		  	$args = array(
+		  		'post_status' => 'inherit', 
+		  		'post_type' => 'attachment', 
+		  		'post_mime_type' => 'image', 
+		  		'order' => 'ASC', 
+		  		'orderby' => 'menu_order ID'
+		  		);
+		  	
+		  	$icons = new WP_Query( $args );
 
-	function form( $instance ){
-		extract($instance);
-	}
+		  	if( $icons->have_posts() ) {
+				while ($icons->have_posts()) {
+					$icons->the_post();	
+					$thumb_image = get_post_thumbnail_id();			
+					$thumb_img_url = wp_get_attachment_url( $thumb_image, 'full' );
+					$return[] = array('id'=>get_post_thumbnail_id(),'src'=>$thumb_img_url,'name'=>get_the_title());
+				} // while
+			} // if have posts
 
-	function widget( $args , $instance ){
-		extract($args);
-		extract($instance);
-	}
+			wp_reset_query();
+
+			foreach ($return as $iconSrc) {echo '<option value="'.$iconSrc['id'].'">'.$iconSrc['name'].'</option>';}
+
+		?>
+          
+        </select>                
+      </label>
+     </p>
+     <!-- Widget City field END -->
+     <?php
+
+
+   
+
+  }
 
 }
 
--- learnings-morning/wp-content/themes/twentyfifteen/functions.php
+++ learnings-morning/wp-content/themes/twentyfifteen/functions.php
@@ -353,3 +353,11 @@ require get_template_directory() . '/inc/template-tags.php';
  * @since Twenty Fifteen 1.0
  */
 require get_template_directory() . '/inc/customizer.php';
+
+
+function wptp_add_categories_to_attachments() {
+    register_taxonomy_for_object_type( 'category', 'attachment' );
+}
+add_action( 'init' , 'wptp_add_categories_to_attachments' );
+
+ 

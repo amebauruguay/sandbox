8c970bda84ccc9a9f29eb0bdef3199211bd110f8
Ligia <Mazzilli>
2015-06-26 16:34:21 -0300

    Ligia

--- LerningMornings/wp-content/plugins/widget/primer_widget.php
+++ LerningMornings/wp-content/plugins/widget/primer_widget.php
@@ -32,14 +32,6 @@ class nuestro_primer_widget extends WP_Widget{
 			name= "<?php echo $this -> get_field_name ('campo_texto');?>"
 			value= "<?php if (isset ($campo_texto))echo esc_attr($campo_texto); ?>"/>
 		</p>
-<!-- 		<p>
-			<label for="<?php //echo $this -> get_field_id ('campo_icono');?>">URL del icono: </label>
-			<input
-			class="widefat" 
-			id= "<?php //echo $this -> get_field_id ('campo_icono');?>"
-			name= "<?php //echo $this -> get_field_name ('campo_icono');?>"
-			value="<?php //if (isset ($campo_icono))echo esc_attr($campo_icono); ?>"/>
-		</p> -->
 		<p>
 			<label for="<?php echo $this -> get_field_id ('text');?>">Icono: 
 			<select
@@ -47,9 +39,52 @@ class nuestro_primer_widget extends WP_Widget{
 			name="<?php echo $this -> get_field_name ('campo_icono');?>" 
 			id= "<?php echo $this -> get_field_id ('campo_icono');?>"
 			type="text">
-				<option value='value-one'<?php echo ($campo_icono=='value-one')?'selected':''; ?>>one</option>
-				<option value='value-two'<?php echo ($campo_icono=='value-two')?'selected':''; ?>>two</option>
-				<option value='value-three'<?php echo ($campo_icono=='value-three')?'selected':''; ?>>three</option>
+			<?php
+					/**
+				 	 * The WordPress Query class.
+				 	 * @link http://codex.wordpress.org/Function_Reference/WP_Query
+				 	 *
+				 	 */
+
+					//le pedimos argumentos (datos) a la DB (base de datos). Le estamos pidiendo las imagenes (que son post) con categoria iconos.
+					//Como la categoria de las imagenes tiene un nombre que no es 'Category' significa q tiene otra taxonomy, asi q hay que llamar diferente usando 'tax_query'
+					//A el array de tax_query le pedimos como argumentos el nombre de la taxonomy, el campo (field) del que vamos a tomar el nombre de la category, en esta caso es el slug
+				 	   $args = array(
+				 	    'post_status' => 'inherit', //es necesario para que te musetre la img (no se que es inherit, aparte de heredado)
+				 	    'post_mime_type' => 'image', //es el que dice que queremos los posts imagenes
+				 	    'order' => 'ASC', //es para que te ordene los posts de menor a mayor (ASC)
+				 	    'orderby' => 'menu_order ID', // le dice que los ordene por el id
+				 	    'tax_query' => array(
+								array(
+									'taxonomy' => 'image-category',
+									'field'    => 'slug',
+									'terms'    => 'iconos',
+								),
+							),
+				 	    );
+				 
+				 $loop = new WP_Query( $args ); //llamado a la DB
+				// $iconoID = get_post_ID();
+
+				// The Loop
+				if ( $loop->have_posts() ) {
+					while ( $loop->have_posts() ) {
+						$loop->the_post();
+						//guardamos el id del post(es decir de la imag) en una variable
+						$postID = get_the_id();
+						//guardamos la url de la imagen en una variable
+						$iconoSRC = wp_get_attachment_url( $postID );
+						//mostramos las imagenes dentro del select
+						echo '<option value=' . $iconoSRC . ' > ' . get_the_title() . '</option>';
+					}
+				} else {
+					// no posts found 
+					echo '<option>no posts found</option>';
+				}
+				/* Restore original Post Data */
+				wp_reset_postdata();	
+				
+			?>
 			</select>
 			</label>
 			<!--que llame a todas las imagenes con la categoria icono
@@ -77,12 +112,6 @@ class nuestro_primer_widget extends WP_Widget{
 <?php
 	
 	}
- // function update($new_instance, $old_instance) {
- //    	$instance = $old_instance;
- //    	$instance['title'] = $new_instance['title'];
- //    	$instance['campo_icono'] = $new_instance['campo_icono'];
- //    	return $instance;
- //  	}
 	public function widget($args, $instance)
 	{
 		extract($args);
@@ -95,8 +124,7 @@ class nuestro_primer_widget extends WP_Widget{
 
 		echo $before_widget;
 			echo "<a href='$campo_link' target=$campo_ventana>";
-			// echo "<img src='$campo_icono'/>";
-			echo 'You selected '. $campo_icono;
+			echo "<img src='$campo_icono'/>";
 			echo $before_title. $campo_texto. $after_title;
 			echo "</a>";
 		echo $after_widget;
